e467b17444fd277b00c2041ed336dd36431e4f4e
Andrej <138933791+Andrej-Clarey@users.noreply.github.com>
2023-08-10 11:19:25 -0400

    3 exer

---
+++ part3/exercises-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HobbyIntroduction from './components/Introduction';
+
+function App() {
+  return (<div>
+   <h1>My Hobby:Diablo 4</h1>
+   <HobbyIntroduction/>
+   </div>
+  );
+}
+
+export default App;
---
+++ part3/exercises-app/src/components/Introduction.js
@@ -0,0 +1,12 @@
+
+
+export default function HobbyIntroduction(){
+    return(<div>
+        <h2>3 Fun Facts About Diablo 4</h2>
+        <ol>
+            <li>100s of hours of gameplay</li>
+            <li>1000s of unique builds and equipment</li>
+            <li>Lots of endgame content</li>
+        </ol>
+    </div>);
+}
